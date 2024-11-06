5b50cab6fb3500dabb06b49ea7c1401da5cfbb41
MrHarte <111158364+MrHarte@users.noreply.github.com>
2024-11-06 12:35:55 -0500

    Added BigCat into Animal hierarchy

---
+++ src/main/java/com/example/oop2a4zms/Model/BigCat.java
@@ -0,0 +1,7 @@
+package com.example.oop2a4zms.Model;
+
+public class BigCat extends Animal {
+    public BigCat(String pName, int pAge, String pSex, double pWeight) {
+        super(pName, pAge, pSex, pWeight);
+    }
+}
