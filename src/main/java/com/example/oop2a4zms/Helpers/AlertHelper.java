5fb33169f307200af1359aa6ff38e9eaaa417c1e
Moustapha <MamadouMoustapha.Cou@stu.crc-lennox.qc.ca>
2024-11-10 19:22:11 -0500

    - used object oriented approach writing convention/added error handling
    -modified the showConfirmation method to handle boolean
    
    -removed unnecessary code

--- src/main/java/com/example/oop2a4zms/Helpers/AlertHelper.java
+++ src/main/java/com/example/oop2a4zms/Helpers/AlertHelper.java
@@ -1,6 +1,8 @@
 package com.example.oop2a4zms.Helpers;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import java.util.Optional;
 
 /**
  * A helper class for displaying alert messages in a JavaFX application.
@@ -20,11 +22,14 @@ public class AlertHelper {
 
     /**
      * Displays a confirmation alert dialog with the specified message.
+     * Returns true if the user confirms, false otherwise.
      *
      * @param pMessage The message to be displayed in the confirmation alert dialog.
+     * @return boolean indicating user's confirmation choice
      */
-    public static void showConfirmation(String pMessage) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, pMessage);
-        alert.showAndWait();
+    public static boolean showConfirmation(String pMessage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, pMessage, ButtonType.YES, ButtonType.NO);
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.YES;
     }
 }
